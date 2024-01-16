851ebfd687f78bae0574b7bab4966e3e197bf22a
kali <osemafadhel41@gmail.com>
2024-01-16 20:08:56 +0100

    cpp

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,45 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 19:59:45 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/01/16 19:59:46 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/01/16 20:07:30 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					argv[i][j] -= 32;
+				j++;
+			}
+			i++;
+		}
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << argv[i][j];
+				j++;
+			}
+			i++;
+			std::cout << " ";
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,39 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 19:59:25 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/01/16 19:59:26 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/01/16 20:08:12 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+class PhoneBook
+{
+	private:
+		Contact contacts[8];
+		int index;
+};
+
+class Contact
+{
+	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string	phone_number;
+		std::string darkest_secret;
+	public:
+		Contact();
+		~Contact();
+		void add();
+		void search();
+		void print();
+
+};
+
+int main()
+{
+	return 0;
+}
