e120610d015c9fd2d8b53a568407180ffe666598
ofadhel <osemafadhel41@gmail.com>
2024-07-17 15:11:04 +0200

    to finish cpp07

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 14:56:29 by ofadhel           #+#    #+#             */
+/*   Updated: 2024/07/17 15:09:16 by ofadhel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+class Array {
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array& other);
+		Array& operator=(const Array& other);
+		~Array();
+
+		T& operator[](unsigned int index);
+		const T& operator[](unsigned int index) const;
+		unsigned int size() const;
+
+	private:
+		T* elements;
+		unsigned int length;
+};
+
+#endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 14:56:17 by ofadhel           #+#    #+#             */
+/*   Updated: 2024/07/17 15:07:45 by ofadhel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Array.hpp"
+
+int main() {
+	try {
+		Array<int> arr1;
+		std::cout << "Default constructor, size: " << arr1.size() << std::endl;
+
+		Array<int> arr2(5);
+		std::cout << "Constructor with size 5, size: " << arr2.size() << std::endl;
+
+		Array<int> arr3(arr2);
+		std::cout << "Copy constructor, size: " << arr3.size() << std::endl;
+
+		Array<int> arr4;
+		arr4 = arr3;
+		std::cout << "Assignment operator, size: " << arr4.size() << std::endl;
+
+		for (unsigned int i = 0; i < arr2.size(); ++i) {
+			arr2[i] = i * 10;
+			std::cout << "arr2[" << i << "]: " << arr2[i] << std::endl;
+		}
+		try {
+			std::cout << arr2[10] << std::endl;
+		} catch (const std::out_of_range& e) {
+			std::cout << "Exception caught: " << e.what() << std::endl;
+		}
+	} catch (const std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	return 0;
+}
