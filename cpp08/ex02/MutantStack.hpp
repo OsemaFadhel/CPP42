0dfe756a3d464e56540069570f9990889bc6219b
ofadhel <osemafadhel41@gmail.com>
2024-07-22 18:29:38 +0200

    cpp08tofinish

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 18:26:32 by ofadhel           #+#    #+#             */
+/*   Updated: 2024/07/22 18:28:47 by ofadhel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <deque>
+
+template <typename T>
+class MutantStack : public std::stack
+{
+
+};
+
+#endif
