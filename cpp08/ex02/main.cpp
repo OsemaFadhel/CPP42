135fbc2728905792469982355fe1789865217d51
ofadhel <osemafadhel41@gmail.com>
2024-07-25 15:31:52 +0200

    cpp08 done

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 18:17:26 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/07/25 15:15:31 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/07/25 15:31:17 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int main()
 {
+	std::cout << "MutantStack Subject Test" << std::endl;
+
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -40,6 +42,35 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+
+	std::cout << "COMPARE Test With LIST" << std::endl;
+
+	std::list<int> mlist;
+
+	mlist.push_back(5);
+	mlist.push_back(17);
+
+	std::cout << mlist.back() << std::endl;
+	mlist.pop_back();
+
+	std::cout << mlist.size() << std::endl;
+
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
+
+	std::list<int>::iterator itl = mlist.begin();
+	std::list<int>::iterator itel = mlist.end();
+
+	++itl;
+	--itl;
+	while (itl != itel)
+	{
+		std::cout << *itl << std::endl;
+		++itl;
+	}
 
 	return 0;
 }
