88d4131e643a924ea3c7416d55bca2a9a19ec25d
ofadhel <osemafadhel41@gmail.com>
2024-07-25 12:46:45 +0200

    cpp

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:25:38 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/07/22 18:11:55 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/07/25 12:45:10 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,40 @@
 
 int main()
 {
-	Span sp = Span(5);
+	try
+	{
+		Span sp = Span(5);
 
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
 
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
 
-	std::cout << "extra test addnumber" << std::endl;
-	
+		std::cout << "extra test addnumber" << std::endl;
+
+		int array[] = {6, 3, 17, 9, 11};
+		std::vector<int> vect(array, array + 5);
+
+		Span sp2 = Span(5);
+
+		sp2.addNumber(vect.begin(), vect.end());
+
+		std::cout << sp2.shortestSpan() << std::endl;
+		std::cout << sp2.longestSpan() << std::endl;
+
+		sp.addNumber(vect.begin(), vect.end());
+
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
 	return 0;
 }
